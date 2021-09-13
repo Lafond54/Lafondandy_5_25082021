9b1f895928cae8cc7518883fa5ba5b001dd46869
Andy <lafond.andy@gmail.com>
2021-09-13 19:42:10 +0200

    premier jet page produit.js + essai fonction conversion centimes => euros

--- public/script/index.js
+++ public/script/index.js
@@ -2,8 +2,10 @@
 //const teddies = [{ "colors": ["Tan", "Chocolate", "Black", "White"],
 // "_id": "5be9c8541c9d440000665243", "name": "Norbert", "price": 2900, "imageUrl": "http://localhost:3000/images/teddy_1.jpg", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }, { "colors": ["Pale brown", "Dark brown", "White"], "_id": "5beaa8bf1c9d440000a57d94", "name": "Arnold", "price": 3900, "imageUrl": "http://localhost:3000/images/teddy_2.jpg", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }, { "colors": ["Brown"], "_id": "5beaaa8f1c9d440000a57d95", "name": "Lenny and Carl", "price": 5900, "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "imageUrl": "http://localhost:3000/images/teddy_3.jpg" }, { "colors": ["Brown", "Blue", "Pink"], "_id": "5beaabe91c9d440000a57d96", "name": "Gustav", "price": 4500, "imageUrl": "http://localhost:3000/images/teddy_4.jpg", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }, { "colors": ["Beige", "Tan", "Chocolate"], "_id": "5beaacd41c9d440000a57d97", "name": "Garfunkel", "price": 5500, "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "imageUrl": "http://localhost:3000/images/teddy_5.jpg" }]
 
+
+
 //variable http de l'API
-let teddies = "http://localhost:3000/api/teddies"
+const teddiesApiUrl = "http://localhost:3000/api/teddies"
 
 //variable constante nommé templatearticle cible la div template 
 const templatearticle = document.querySelector("#templatearticle").content
@@ -14,25 +16,65 @@ console.log(templatearticle)
 const articles = document.querySelector(".articles")
 console.log(articles)
 
-//Appel de l'API avec fetch
-fetch(teddies)
-    .then(function (res) {                                      // Promesses
-        if (res.ok) {
-            return res.json();
+//fonction 
+async function getTeddies() {
+    try {
+        const res = await fetch(teddiesApiUrl)
+        if (!res.ok) {
+            throw res
         }
-    })
-    .then(function (teddies) {                                   // <== teddies entre les parenthèses ?     
-        teddies.forEach(teddy => {                             //Boucle qui permet d’itérer sur les propriétés d’un tableau et afficher les infos envoyés par l'API sur les articles de la home page
-            const article = templatearticle.cloneNode(true)
-            article.querySelector(".article__image").src = teddy.imageUrl
-            article.querySelector(".article__titre").innerText = teddy.name
-            article.querySelector(".article__soustitre").innerText = teddy.price
-            articles.appendChild(article)
-        })
-    })
-    .catch(function (err) { "erreurrrrr" })
+        return await res.json();
+    }
+    catch (err) {
+        console.error(err)
+    }
+}
+
+// fonction
+
+function addTeddyToDom(teddy) {
+    const article = templatearticle.cloneNode(true)
+    article.querySelector(".article__image").src = teddy.imageUrl
+    article.querySelector(".article__titre").innerText = teddy.name
+    article.querySelector(".article__soustitre").innerText = teddy.price
+    article.querySelector(".article").setAttribute("href", "produit.html?id=" + teddy._id)
+    articles.appendChild(article)
+}
 
 
+// ******************************
+// Conversion Centimes en Euros
+const article = templatearticle
+let prix = article.querySelector(".article__soustitre").innerText // Surement Mauvais ciblage 
+let valeurPrix = prix.value // Undefined
+console.log(valeurPrix)
+
+const b = 100
+function conversionEuro(a, b) {
+    let c = a / b;
+    console.log(c)        // NaN
+}
+
+conversionEuro(prix, b) 
+
+// ******************************
+
+
+//fonction
+async function refresh() {
+    articles.innerHTML = ""
+    const teddies = await getTeddies()
+    teddies.forEach(addTeddyToDom)
+
+
+}
+
+refresh()
+/*
+//teddies.forEach(teddy => {
+    addTeddyToDom(teddy)
+})
+*/
 
 
 
---
+++ public/script/produit.js
@@ -0,0 +1,104 @@
+//API local
+//const teddies = [{ "colors": ["Tan", "Chocolate", "Black", "White"],
+// "_id": "5be9c8541c9d440000665243", "name": "Norbert", "price": 2900, "imageUrl": "http://localhost:3000/images/teddy_1.jpg", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }, { "colors": ["Pale brown", "Dark brown", "White"], "_id": "5beaa8bf1c9d440000a57d94", "name": "Arnold", "price": 3900, "imageUrl": "http://localhost:3000/images/teddy_2.jpg", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }, { "colors": ["Brown"], "_id": "5beaaa8f1c9d440000a57d95", "name": "Lenny and Carl", "price": 5900, "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "imageUrl": "http://localhost:3000/images/teddy_3.jpg" }, { "colors": ["Brown", "Blue", "Pink"], "_id": "5beaabe91c9d440000a57d96", "name": "Gustav", "price": 4500, "imageUrl": "http://localhost:3000/images/teddy_4.jpg", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }, { "colors": ["Beige", "Tan", "Chocolate"], "_id": "5beaacd41c9d440000a57d97", "name": "Garfunkel", "price": 5500, "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "imageUrl": "http://localhost:3000/images/teddy_5.jpg" }]
+
+
+
+// ******************************
+//Obtention de l'ID de la page
+function getParameter(paramaterId) {
+    let parameters = new URLSearchParams(window.location.search);
+    return parameters.get(paramaterId);
+}
+console.log(window.location.search)
+
+//Variable Chaine de caractere id isolé
+let id = getParameter("id")
+console.log(id)
+
+//Variable http de l'API
+let teddiesApiUrl = "http://localhost:3000/api/teddies"
+
+//Variable egale à Concatenation Url + ID
+let teddiesApiId = teddiesApiUrl.concat(`/` + id)
+console.log(teddiesApiId)
+
+// Test réponse de l'api (a supprimer)
+fetch(teddiesApiId)
+.then (res => res.json())
+.then (data => console.log(data))
+
+
+
+// ******************************
+//Variable constante nommé card cible  #templatecard
+const templatecard = document.querySelector(".card").content
+console.log(".card")
+
+//Variable constante cible la div qui recoit  le futur article
+const articles = document.querySelector(".articles")
+console.log(articles)
+
+
+//fonction 
+async function getTeddies() {
+    try {
+        const res = await fetch(teddiesApiId)
+        if (!res.ok) {
+            throw res
+        }
+        return await res.json();
+    }
+    catch (err) {
+        console.error(err)
+    }
+}
+
+
+// fonction
+
+function addTeddyToDom(teddy) {
+    const article = templatecard.cloneNode(true)
+    article.querySelector(".card__divimg__item").src = teddy.imageUrl
+    article.querySelector(".card__d__name").innerText = teddy.name
+    article.querySelector(".card__d__price").innerText = teddy.price
+    article.querySelector(".card__d__descr").innerText = teddy.description
+    //article.querySelector(".article").setAttribute("href", "produit.html?id=" + teddy._id)
+    articles.appendChild(article)
+
+}
+
+
+
+async function refresh() {
+    articles.innerHTML = ""
+    const teddies = await getTeddies()
+    
+
+
+
+}
+
+refresh()
+
+
+
+
+
+
+
+
+
+
+
+/*
+//teddies.forEach(teddy => {
+    addTeddyToDom(teddy)
+})
+*/
+
+
+
+
+
+
