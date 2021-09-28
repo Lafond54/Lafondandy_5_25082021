a1b3109fb7c365bf7a18868a399608d824bfdfbb
Andy <lafond.andy@gmail.com>
2021-09-29 00:04:46 +0200

    email regExp + misc essai

--- public/script/commande.js
+++ public/script/commande.js
@@ -3,6 +3,48 @@
 const panier = JSON.parse(localStorage.getItem("panier"))
 console.log(panier)
 
+// Vider le panier
+
+    document.querySelector(".clearpanier").addEventListener('click', () => {
+
+        localStorage.clear()
+        location.reload(true) // Bonne methode pour refresh la page ?
+
+    })
+
+// //Variable http de l'API
+// let teddiesApiUrl = "http://localhost:3000/api/teddies"
+
+// //Variable egale à Concatenation Url + ID
+// let teddyApiUrl = teddiesApiUrl.concat(`/` + id)
+
+// Boucle sur le tableau d'ojets du LS pour obtenir la liste des IDs dans le chariot
+for (i = 0; i < panier.length; i++) {
+    let chariot = panier[i]
+    console.log(chariot)   
+
+}
+
+
+// 
+// async () => {
+//     const products = await Promise.all(
+//         cartProducts
+//         .reduce((acc, cartProduct) => {
+//             acc.add(cartProduct.id)
+//         }, new Set())
+//         .map(getProductData)
+//     )
+
+
+
+
+
+
+
+
+
+
 
 //********affichage des produits du panier**********
 const elementPanier = document.querySelector(".tableaupanier__body")
@@ -31,11 +73,79 @@ else {
         `
 
     }
-    if( i== panier.length) {
+    if (i == panier.length) {
         //injection html dans page panier
-        
+
         elementPanier.innerHTML = structureProduitPanier
-        console.log(structureProduitPanier)
+
     }
-    
+
 }
+
+
+
+
+// Formulaire
+
+let form = document.querySelector('#purchaseform')
+
+
+//ecouter la modification de l'email
+
+form.email.addEventListener('change', function () {
+
+    validEmail(this)
+})
+
+const validEmail = function (inputEmail) {
+    // Creation de la RegExp pour la validation email
+    let emailRegExp = new RegExp('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$', 'g')
+    //Confrontation mail avec les regles du Regexp renvoit du booleen
+    let testEmail = emailRegExp.test(inputEmail.value)
+    console.log(testEmail)
+    // selectionner balise small
+    let small = inputEmail.nextElementSibling
+    // Message dans la balise small selon le resultat booleen precedent (ligne62)
+    if (testEmail == true) {
+        small.innerHTML = "✔ Adresse Valide"
+        small.classList.remove('__notok')    // Enlever couleur precedente
+        small.classList.add('__ok')          // couleur message de validation
+    } else {
+        small.innerHTML = "✗ Adresse Non Valide"
+        small.classList.remove('__ok')
+        small.classList.add('__notok')
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// const promise1 = Promise.resolve(3);
+// const promise2 = 42;
+// const promise3 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 100, 'foo');
+// });
+
+// Promise.all([promise1, promise2, promise3]).then((values) => {
+//     console.log(values);
+// });
+// // expected output: Array [3, 42, "foo"]
+
+//  //////////////////////////////////////////////////////////////////////
+
+// async () => {
+//     const products = await Promise.all(
+//         cartProducts
+//         .reduce((acc, cartProduct) => {
+//             acc.add(cartProduct.id)
+//         }, new Set())
+//         .map(getProductData)
+//     )
